2ecac1df14c0cb46bd809367b9a30aeb3cfad6ac
djuka991 <kosanovic.djura@gmail.com>
2020-03-23 15:51:39 +0100

    Made card component and export to App.js

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Card.css'
+
+const card = () => {
+ 
+  return(
+    <div className="container">
+        <h3>Belgrade</h3>
+        <img className="container__img" src="https://lh3.googleusercontent.com/proxy/lRKDYofjh-1qQyq42PZ5S7fVzkhfP11sRYY_CmhTOuZsFRhLtKRYitp_xAjtrf6gjz-oLyW06RYtM2t9XpUs1vKY07UMYtIit-v3_vbq_bASPc2x1yejK3aalA" alt=""/>
+        <h3>23c / 15%</h3>
+      </div>
+  );
+};
+
+export default card;
