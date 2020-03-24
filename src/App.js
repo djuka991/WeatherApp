c65b0611376ad582391f8a679b99460abc39f4f3
djuka991 <kosanovic.djura@gmail.com>
2020-03-24 13:23:06 +0100

    Add mock data, use map method to iterate array

--- src/App.js
+++ src/App.js
@@ -1,32 +1,51 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import Card  from './Card'
 import './App.css';
 
 
 function App() {
 
-  useEffect(()=>{
-    console.log("Ovde dodju DATA koja hendlujemo");
-    // getData();
-    
-  },[]);
-
-  const getData = async () => {
-    const response = fetch('http.');
-    const data = await response.json();
-    console.log(data);
-    
-  }
-
+  const places =
+            [
+              {
+                'location' : 'Belgade',
+                'img'      : 'https://cdn1.iconfinder.com/data/icons/weather-forecast-meteorology-color-1/128/weather-sunny-512.png' ,
+                'temperture' : 24 
+              } ,  
+              {
+                'location' : 'Novi Sad',
+                'img'      : 'https://cdn1.iconfinder.com/data/icons/weather-forecast-meteorology-color-1/128/weather-sunny-512.png' ,
+                'temperture' : 24 
+              } ,   
+              {
+                'location' : 'Niš',
+                'img'      : 'https://cdn1.iconfinder.com/data/icons/weather-forecast-meteorology-color-1/128/weather-sunny-512.png' ,
+                'temperture' : 21 
+              } ,  
+              {
+                'location' : 'Subotica',
+                'img'      : 'https://cdn1.iconfinder.com/data/icons/weather-forecast-meteorology-color-1/128/weather-sunny-512.png' ,
+                'temperture' : 20
+              } ,    
+              {
+                'location' : 'Čačak',
+                'img'      : 'https://toppng.com/uploads/preview/sun-with-clouds-transparent-png-picture-sun-with-clouds-11563244598qejhismzny.png',
+                'temperture' : 19 
+              }                                 
+            ]
+             
   return (
     <div className="App">
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+     {places.map(place => (
+       <Card location={place.location}
+             img={place.img}
+             temperture={place.temperture}
+       />
+     ))}
     </div>
   );
-};
+  
+     }
+
 
 export default App;
--- src/Card.js
+++ src/Card.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import './Card.css'
+import './Card.css';
 
-const card = () => {
+const card = ({location, img ,temperture}) => {
  
   return(
     <div className="container">
-        <h3>Belgrade</h3>
-        <img className="container__img" src="https://lh3.googleusercontent.com/proxy/lRKDYofjh-1qQyq42PZ5S7fVzkhfP11sRYY_CmhTOuZsFRhLtKRYitp_xAjtrf6gjz-oLyW06RYtM2t9XpUs1vKY07UMYtIit-v3_vbq_bASPc2x1yejK3aalA" alt=""/>
-        <h3>23c / 15%</h3>
+        <h3>{location}</h3>
+        <img className="container__img" src={img} alt=""/>
+        <h3>{temperture}</h3>
       </div>
   );
 };
